1d0903608e8128e50abca8c1ba4b9939af637301
karolmuniz27 <eukamudejesus@gmail.com>
2025-05-22 11:15:25 -0300

    aqq eu vi let,console e fiz uma calculadora

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,1 +1,28 @@
-console.log ("oi")
+/*let x =1;
+let y =2;
+let som =x+y;
+console.log("A soma é:"+som)
+console.log("                 ")
+
+let subitr =1;
+let sub=2;
+let subitração = 2-1;
+console.log("A subitração é:"+subitração)
+console.log("                 ")
+
+let muti =3;
+let mutii=5;
+let mutipli=15;
+console.log("A multiplicação é:"+mutipli)
+console.log("                 ")
+
+let div=10;
+let divi=2;
+let divisão=5;
+console.log ("A divisão é:"+divisão)*/
+
+
+let pessoa= "Jurema";
+let salario =2000;
+let meses_trabalhados=9;
+console.log ("Parabéns " +pessoa+" seu Decímo é R$:" +salario/12*meses_trabalhados)
