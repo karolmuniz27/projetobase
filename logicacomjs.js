f0bd4f280115e697e0f181015702bc30bac0d6a7
karolmuniz27 <eukamudejesus@gmail.com>
2025-06-03 11:33:55 -0300

    ninhode if

--- logicacomjs.js
+++ logicacomjs.js
@@ -22,7 +22,58 @@ let divisão=5;
 console.log ("A divisão é:"+divisão)*/
 
 
-let pessoa= "Jurema";
+/*let pessoa= "Jurema";
 let salario =2000;
 let meses_trabalhados=9;
 console.log ("Parabéns " +pessoa+" seu Decímo é R$:" +salario/12*meses_trabalhados)
+
+
+let salario = Number (prompt("digite seu salário"))
+let meses_trabalhados = Number (prompt("digite seus meses trabalhados"))
+let resultado = ("salario/12*meses_trabalhado")
+alert(" Seu Decímo é R$" +salario/12*meses_trabalhados)*/
+/*
+let salario =500
+quinze = 15/100*salario+salario
+dez = 10/100*salario+salario
+cinco = 5/100*salario+salario
+
+if =(salario<=500)
+{console.log("seu reajudte é de 15%,logo seu salario é: "+quinze)}
+else if=(salario>500 && salario <=1000){console.log("seu reajudte é de 10%,logo seu salario é: "+dez)}
+else ={console.log("seu reajudte é de 5%,logo seu salario é: "+cinco)}*/
+
+/*
+let idade = prompt("Digite aqui a sua idade: ")
+if(idade<12){alert("Assista conteúdo infantil")}
+else if (idade>=12 && idade<=17)
+{alert("Assista conteúdo teen")}
+else if ( idade>=18 && idade<=60)
+{alert("Assista conteúdo adulto")}
+else {alert("Assista conteúdo Sênior")}
+*/
+/*let Pontuacao = prompt ("Digite sua pontuação")
+if(Pontuacao<=1000)
+    {alert("você é Novato e tem 100XP")}
+else if (Pontuacao>=1001 && Pontuacao<=5000){alert("você é Aprendiz e tem 500XP")}
+else if (Pontuacao>=5001 && Pontuacao<=10000){alert("você é Expert e tem 1000XP")}
+else{ alert ("Você é Mestre e tem 2000XP")}*/
+
+let basico= 25;
+let premium=40;
+let ultra=60;
+let assinatura_tipo = prompt("digite seu tipo de assinatura")
+let quantidade_telas = prompt("sua quantidade de telas")
+if (assinatura_tipo==basio) {
+    if (quantidade_telas >=1 && quantidade_telas<=2)alert("seu plano terá o valor de R$: "+basico)
+}
+
+
+
+
+
+
+
+
+
+
