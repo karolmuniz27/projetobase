4c35e491e221610d126190fb8d7a9c08dafd5aa9
karolmuniz27 <eukamudejesus@gmail.com>
2025-06-12 11:32:23 -0300

    documento e eventes

---
+++ index.js
@@ -0,0 +1,11 @@
+/*let buscando = document.getElementsByTagName("p")[0]
+console.log(buscando)
+
+let buscando= document.getElementsByName("h2dadiv")[0]
+console.log(buscando)
+
+let buscando= document.getElementById("atalaia")
+buscando.style.background="gray"            
+ou : GetElementsClassName()
+let modorapido= document.querySelector("div#")
+*/
